b9f397292fda3f95206439331874e64edb21c069
PabloRS1420 <36885631+PabloRS1420@users.noreply.github.com>
2018-04-29 11:55:22 +0200

    Create p4_e2.c

---
+++ p4_e2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+#include "tree.h"
+#include "types.h"
+
+int main(int argc, char const *argv[]){
+    int x;
+    printf("> ./p4_e2 numeros.txt");
+    printf("Numero de nodos: ");
+    printf("Profundidad: ");
+    printf("> Introduzca un numero: ");
+    scanf("%d", &x);
+    printf("Numero introducido: %d", x);
+    if() {
+        printf("El dato %d se encuentra dentro del Arbol", x);
+    }
+    else {
+        printf("El dato %d no se encuentra dentro del Arbol", x);
+    }
+    
+}
