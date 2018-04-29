4f6a609c0584a9328698342fceee0aa659e160c0
PabloRS1420 <36885631+PabloRS1420@users.noreply.github.com>
2018-04-29 12:07:30 +0200

    Update p4_e2.c

--- p4_e2.c
+++ p4_e2.c
@@ -6,15 +6,29 @@
 #include "tree.h"
 #include "types.h"
 
+
 int main(int argc, char const *argv[]){
     int x;
+    Tree *pa = NULL;
+    FILE *fp = fopen("argv[1]", "r+");
+    if(fp == NULL) {
+        printf("Error abriendo el archivo");
+        return -1;
+    }
+    
+    while(fscanf(fp, "%d", &x)){
+        tree_insert(pa, x);
+    }
+    
     printf("> ./p4_e2 numeros.txt");
-    printf("Numero de nodos: ");
-    printf("Profundidad: ");
+    x = tree_numNodes(pa);
+    printf("Numero de nodos: %d", x);
+    x = tree_depth(pa);
+    printf("Profundidad: %d", x);
     printf("> Introduzca un numero: ");
     scanf("%d", &x);
     printf("Numero introducido: %d", x);
-    if() {
+    if(tree_find(pa, x) == TRUE) {
         printf("El dato %d se encuentra dentro del Arbol", x);
     }
     else {
