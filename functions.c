ab2276323abe1d54c084402760a7a68da9d9170e
PabloRS1420 <36885631+PabloRS1420@users.noreply.github.com>
2018-05-03 13:54:40 +0200

    Update functions.c

--- functions.c
+++ functions.c
@@ -20,7 +20,7 @@ int print_element_function(FILE *f, const void *e) {
     return -1; 
 }
 
-int cmp_elementlist_function(const void *e1, const void *e2) {
+int cmp_element_function(const void *e1, const void *e2) {
     if(e1>e2) return 1;
     else if(e1<e2) return -1;
     else return 0;
