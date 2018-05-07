ab45f30be640b89209ce1c32c57b2d57dec84ad4
PabloRS1420 <36885631+PabloRS1420@users.noreply.github.com>
2018-05-07 20:18:51 +0200

    Update point.h

--- point.h
+++ point.h
@@ -31,35 +31,35 @@ typedef struct _Point Point;
 si no */
 Point *point_ini(int x, int y, char s);
 /* Libera la memoria dinámica reservada para un punto*/
-void point_destroy(Point *);
+void point_destroy(Point *p);
 /* Devuelve la coordenada X de un punto dado, o -1 si se produce algún error */
-int point_getCoordinateX(const Point *);
+int point_getCoordinateX(const Point *p);
 /* Devuelve la coordenada Y de un punto dado, o -1 si se produce algún error */
-int point_getCoordinateY(const Point *);
+int point_getCoordinateY(const Point *p);
 /* Devuelve el símbolo de un punto dado, o ERRORCHAR si se produce algún error */
-char point_getSymbol(const Point *);
+char point_getSymbol(const Point *p);
 /* Funciones derivadas que indican el tipo de punto que tenemos */
-Bool point_isInput(Point *); 
-Bool point_isOutput(Point *);
-Bool point_isBarrier(Point *);
-Bool point_isVisited(Point *); 
-Bool point_isSpace(Point *);
+Bool point_isInput(Point *p); 
+Bool point_isOutput(Point *p);
+Bool point_isBarrier(Point *p);
+Bool point_isVisited(Point *p); 
+Bool point_isSpace(Point *p);
 /* Modifica la coordenda X de un punto dado, devuelve ERROR si se produce algún error */
-Status point_setCoordinateX(Point *, const int);
+Status point_setCoordinateX(Point *p, const int x);
 /* Modifica la coordenda Y de un punto dado, devuelve ERROR si se produce algún error */
-Status point_setCoordinateY(Point *, const int);
+Status point_setCoordinateY(Point *p, const int y;
 /* Modifica el símbolo de un punto dado, devuelve ERROR si se produce algún error */
-Status point_setSymbol(Point *, const char);
+Status point_setSymbol(Point *p, const char s);
 /* Devuelve TRUE si los dos puntos pasados como argumentos son iguales (revisando todos sus campos).
  Devuelve FALSE en otro caso. */
-Bool point_equals(const Point *, const Point *);
+Bool point_equals(const Point *p1, const Point *p2);
 /* Copia los datos de un punto a otro devolviendo el punto copiado (incluyendo la reserva de la memoria necesaria)
 si todo ha ido bien, o NULL en otro caso */
-Point *point_copy(const Point *);
+Point *point_copy(const Point *p);
 /* Imprime en un fichero dado los datos de un punto con el siguiente formato: [(x,y): symbol]. Por ejemplo, un punto
 con símbolo “*”, con coordenada X 3 e Y 7 se representará como [(3, 7): *]. Además, devolverá el número de
 caracteres que se han escrito con éxito (mirar documentación de fprintf) */
-int point_print(FILE *, const Point *); 
+int point_print(FILE *f, const Point *p); 
 
 
 #ifdef __cplusplus
