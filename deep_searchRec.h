6e33be466d76a6aa98efa4f34ecebe83ce0b652c
PabloRS1420 <36885631+PabloRS1420@users.noreply.github.com>
2018-05-07 19:57:59 +0200

    Create deep_searchRec.h

---
+++ deep_searchRec.h
@@ -0,0 +1,11 @@
+#indef DEEP_SEARCHREC_H
+#define DEEP_SEARCHREC_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "map_solver.h"
+
+Point *deep_searchRec(Map m, Point p_in, Move strategy[4]);
+
+#endif DEEP_SEARCHREC_H
