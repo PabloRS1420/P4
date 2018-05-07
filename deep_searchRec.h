dd12b61166298d2a18f7cbde1c513f0096947558
PabloRS1420 <36885631+PabloRS1420@users.noreply.github.com>
2018-05-07 19:58:48 +0200

    Update deep_searchRec.h

--- deep_searchRec.h
+++ deep_searchRec.h
@@ -6,6 +6,6 @@
 #include <string.h>
 #include "map_solver.h"
 
-Point *deep_searchRec(Map m, Point p_in, Move strategy[4]);
+Point *deep_searchRec(Map *m, Point *p_in, Move strategy[4]);
 
 #endif DEEP_SEARCHREC_H
